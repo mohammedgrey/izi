06833bb1a925c7e349027ee6886b81b5ce4a46ac
mohammedgrey <mohammeddsaadd@yahoo.com>
2020-06-30 09:23:27 +0200

    Adjusted the facebook link in the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ const Footer =()=>(
 <p>Follow us</p>
 <div className="social-media-icons">
 
-<a href="https://www.facebook.com/izi.handmade" target="_blank"><i class="fab fa-facebook-f"></i></a>
+<a href="https://www.facebook.com/izi.handmade/" target="_blank"><i class="fab fa-facebook-f"></i></a>
 
 <a href="https://www.instagram.com/izi.handmade/" target="_blank"><i class="fab fa-instagram"></i></a>
 </div>
