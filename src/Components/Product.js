2059b615fe447d54fdc7cfa738dc8570e3fc8a19
mohammedgrey <mohammeddsaadd@yahoo.com>
2020-07-01 11:45:22 +0200

    Added ordering through whatsapp feature.

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './Product.css'
+import OrderModal from "./OrderModal";
 
 
 
 const Product =(props)=>{
-
-
+  const [modalShow, setModalShow] = React.useState(false);
   return(
     <div className="product-class">
     
@@ -15,6 +15,16 @@ const Product =(props)=>{
             <p>Size: <span>{`${props.size}cm`}</span></p>
             <p>Status: <span>{props.status}</span></p>
             <p>Price: <span>{`${props.price}EGP`}</span></p>
+            <button onClick={()=>setModalShow(true)} className="order-now"><i class="fas fa-shopping-cart"></i><text>Order Now</text></button>
+            {/* <button className="show-details"><i class="fas fa-plus"></i><text>Show Details</text></button> */}
+
+            <OrderModal
+            name={props.name}
+            size={props.size}
+            image={props.image}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
             </div>
     
            
